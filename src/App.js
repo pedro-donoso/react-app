7e78594ea23a2495346296aa4d65363a0cacb8d0
pedro <pedro.donoso.r@usach.cl>
2022-01-18 11:08:31 -0300

    agrego boton

--- src/App.js
+++ src/App.js
@@ -13,7 +13,13 @@ function App() {
   const updateQuote = async () => {
     const url = "https://www.breakingbadapi.com/api/quote/random";
     const res = await fetch(url);
-    const data = await res.json();
+    const [newQuote] = await res.json();
+    const {quote: text, author} = newQuote;
+
+    setQuote({
+      text,
+      author,
+    })
   }
 
   useEffect(() => {
@@ -26,7 +32,7 @@ function App() {
     return (
       <div className='app'>
       <img src='https://upload.wikimedia.org/wikipedia/commons/7/77/Breaking_Bad_logo.svg' alt='logo' />
-      <button>Get Another</button>
+      <button onClick={()=>alert('Click')}>Get Another</button>
 
       <Quote quote={quote}/>
   
